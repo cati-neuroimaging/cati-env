33136d82f6ba8a65cba0f397a62931817b205e55
sapetnioc <sapetnioc@users.noreply.github.com>
2024-12-12 17:02:31 +0100

    Define package list and corresponding main brainvisa-cmake component location

--- soma_dev/__init__.py
+++ soma_dev/__init__.py
@@ -1,1 +1,21 @@
-__version__ = "0.0.1"
+__version__ = "0.0"
+
+packages = {
+    "anatomist": ["https://github.com/brainvisa/anatomist-free.git", "master"],
+    "bioprocessing": ["https://bioproj.extra.cea.fr/git/bioprocessing", "master"],
+    "brainrat": ["https://bioproj.extra.cea.fr/git/brainrat-private", "master"],
+    "capsul": ["https://github.com/populse/capsul.git", "master"],
+    "constellation": ["https://github.com/brainvisa/constellation-gpl.git", "master"],
+    "cortical-surface": ["https://github.com/brainvisa/cortical_surface-gpl.git", "master"],
+    "disco": ["https://bioproj.extra.cea.fr/git/brainvisa-disco", "master"],
+    "highres-cortex": ["https://github.com/neurospin/highres-cortex.git", "master"],
+    "morphologist": ["https://github.com/brainvisa/morphologist-nonfree.git", "master"],
+    "morphologist-baby": ["https://bioproj.extra.cea.fr/git/brainvisa-t1mri/morphologist-baby.git", "master"],
+    "nuclear-imaging": ["https://github.com/cati-neuroimaging/nuclear_imaging-nonfree.git", "master"],
+    "populse-db": ["https://github.com/populse/populse_db.git", "master"],
+    "primatologist": ["https://bioproj.extra.cea.fr/git/primatologist-gpl", "master"],
+    "soma": ["https://github.com/brainvisa/soma-io.git", "master"],
+    "soma-freesurfer": ["https://github.com/brainvisa/brainvisa_freesurfer.git", "master"],
+    "soma-python": ["https://github.com/populse/soma-base.git", "master"],
+    "soma-spm": ["https://github.com/brainvisa/brainvisa-spm.git", "master"],
+}
