faab52b2c031ebdb4401b50b4623a7553cad7beb
sapetnioc <sapetnioc@users.noreply.github.com>
2025-01-02 11:44:14 +0100

    Added graphviz subcommand to soma-dev

--- soma_dev/cli.py
+++ soma_dev/cli.py
@@ -1,9 +1,12 @@
+import fnmatch
 import os
 import pathlib
+import re
 import subprocess
 
 import fire
-import yaml
+
+from .recipes import sorted_recipies, find_soma_dev_packages
 
 
 class Commands:
@@ -40,8 +43,54 @@ class Commands:
     def apply_plan(self):
         raise NotImplementedError()
 
-    def graphviz(self):
-        raise NotImplementedError()
+    def graphviz(self, packages: str = "*", conda_forge=False):
+        """Output a dot file for selected packages (or for all known packages by default)"""
+        selector = re.compile("|".join(f"(?:{fnmatch.translate(i)})" for i in packages))
+        neuro_forge_packages = set()
+        conda_forge_packages = set()
+        linked = set()
+        print("digraph {")
+        print("  node [shape=box, color=black, style=filled]")
+        recipes ={recipe["package"]["name"]: recipe for recipe in sorted_recipies()}
+        selected_recipes = set()
+        stack = [i for i in recipes if selector.match(i)]
+        while stack:
+            package = stack.pop(0)
+            selected_recipes.add(package)
+            recipe = recipes[package]
+            stack.extend(
+                dependency
+                for dependency in recipe["soma-dev"].get("internal-dependencies", [])
+                if dependency not in selected_recipes
+            )
+
+        all_soma_dev_packages = set(find_soma_dev_packages())
+        for package in selected_recipes:
+            recipe = recipes[package]
+            if recipe["soma-dev"]["type"] == "interpreted":
+                print(f'  "{package}" [fillcolor="aquamarine2"]')
+            elif recipe["soma-dev"]["type"] == "compiled":
+                print(f'  "{package}" [fillcolor="darkgreen",fontcolor=white]')
+            elif recipe["soma-dev"]["type"] == "virtual":
+                print(f'  "{package}" [fillcolor="powderblue"]')
+            else:
+                print(f'  "{package}" [fillcolor="bisque"]')
+            for dependency in recipe["soma-dev"].get("internal-dependencies", []):
+                if (package, dependency) not in linked:
+                    print(f'  "{package}" -> "{dependency}"')
+                    linked.add((package, dependency))
+            for dependency in recipe.get("requirements", {}).get("run", []):
+                if dependency in all_soma_dev_packages:
+                    neuro_forge_packages.add(dependency)
+                    print(f'  "{package}" -> "{dependency}"')
+                elif conda_forge:
+                    conda_forge_packages.add(dependency)
+                    print(f'  "{package}" -> "{dependency}"')
+        for package in neuro_forge_packages:
+            print(f'  "{package}" [fillcolor="bisque"]')
+        for package in conda_forge_packages:
+            print(f'  "{package}" [fillcolor="aliceblue"]')
+        print("}")
 
 
 def main():
---
+++ soma_dev/recipes.py
@@ -0,0 +1,91 @@
+import os
+import pathlib
+import yaml
+
+
+def read_recipes():
+    """
+    Iterate over all recipes files defined in soma-forge.
+    """
+    soma_root = pathlib.Path(os.environ["SOMA_ROOT"])
+    for component_src in (soma_root / "src").iterdir():
+        recipe_file = component_src / "soma-dev" / "soma-recipe.yaml"
+        if recipe_file.exists():
+            with open(recipe_file) as f:
+                recipe = yaml.safe_load(f)
+                yield recipe
+
+
+def selected_recipes(selection=None):
+    """
+    Iterate over recipes selected in configuration and their dependencies.
+    """
+    # Read recipes
+    recipes = {r["package"]["name"]: r for r in read_recipes()}
+
+    all_packages = set(recipes)
+
+    metapackages = {
+        "all": all_packages,
+    }
+    if not selection:
+        selection = ["all"]
+    selected_packages = set()
+    for s in selection:
+        if s.startswith("-"):
+            s = s[1:].strip()
+            remove = True
+        else:
+            remove = False
+        m = metapackages.get(s)
+        if m is not None:
+            s = m
+        else:
+            s = {s}
+        if remove:
+            selected_packages = selected_packages.difference(s)
+        else:
+            selected_packages.update(s)
+
+    # Walk over selected packages and dependencies
+    stack = list(selected_packages)
+    done = set()
+    while stack:
+        package = stack.pop(0)
+        if package in done:
+            continue
+        recipe = recipes[package]
+        yield recipe
+        done.add(package)
+        dependencies = recipe["soma-dev"].get("internal-dependencies", [])
+        stack.extend(i for i in dependencies if i not in done)
+
+
+def sorted_recipies():
+    """
+    Iterate over recipes sorted according to their dependencies starting with a
+    package without dependency.
+    """
+    recipes = {r["package"]["name"]: r for r in selected_recipes()}
+    ready = set()
+    inverted_dependencies = {}
+    for package, recipe in recipes.items():
+        dependencies = recipe["soma-dev"].get("internal-dependencies", [])
+        if not dependencies:
+            ready.add(package)
+        for dependency in dependencies:
+            inverted_dependencies.setdefault(dependency, set()).add(package)
+
+    done = set()
+    while ready:
+        package = ready.pop()
+        yield recipes[package]
+        done.add(package)
+        for dependent in inverted_dependencies.get(package, []):
+            dependencies = recipe.get("internal-dependencies", [])
+            if all(d in done for d in dependencies):
+                ready.add(dependent)
+
+def find_soma_dev_packages():
+    for recipe in read_recipes():
+        yield recipe["package"]["name"]
